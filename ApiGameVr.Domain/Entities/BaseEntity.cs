a894838ce06faeca06065fac1166949dffd7184d
VinceCDA <84965037+VinceCDA@users.noreply.github.com>
2026-01-26 16:56:10 +0100

    added base entities

---
+++ ApiGameVr.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiGameVr.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get;private set; }
+        public BaseEntity()
+        {
+            this.ModifiedAt = DateTime.Now;
+        }
+    }
+}
---
+++ ApiGameVr.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ApiGameVr.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public required string Email { get; set; }
+        public required string Pseudo {  get; set; }
+    }
+}
