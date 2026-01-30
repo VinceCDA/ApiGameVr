fe2d56162a0b5732f60d0133eed9c203db0c4733
VinceCDA <84965037+VinceCDA@users.noreply.github.com>
2026-01-30 15:06:47 +0100

    added admin enum

---
+++ ApiGameVr.Domain/Enums/RolesEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ApiGameVr.Domain.Enums
+{
+    [DataContract]
+    public enum Roles
+    {
+        [EnumMember(Value = "moderator")]
+        Moderator,
+
+        [EnumMember(Value = "administrator")]
+        Administrator,
+
+        [EnumMember(Value = "superadministrator")]
+        SuperAdministrator,
+
+    }
+}
