cc4e25647d4edecfe3c6836e60bddd448d0dd38b
VinceCDA <84965037+VinceCDA@users.noreply.github.com>
2026-02-02 11:38:08 +0100

    added architectures tests

---
+++ ApiGameVr.Test/ArchitectureTests/BaseTest.cs
@@ -0,0 +1,19 @@
+﻿using ApiGameVr.Application.Interfaces.Repositories;
+using ApiGameVr.Domain.Entities;
+using ApiGameVr.Infrastructure.Data;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ApiGameVr.Test.ArchitectureTests
+{
+    public abstract class BaseTest
+    {
+        protected static readonly Assembly DomainAssembly = typeof(User).Assembly;
+        protected static readonly Assembly ApplicationAssembly = typeof(IUserRepository).Assembly;
+        protected static readonly Assembly InfrastructureAssembly = typeof(UserRepository).Assembly;
+        protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+    }
+}
