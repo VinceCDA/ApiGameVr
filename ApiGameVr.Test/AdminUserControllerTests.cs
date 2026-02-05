c74185612ce2c483160a1603105ce18f356f4bfd
VinceCDA <84965037+VinceCDA@users.noreply.github.com>
2026-02-05 16:50:44 +0100

    updated test

---
+++ ApiGameVr.Test/AdminUserControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using ApiGameVr.Application.Features.Users.Commands.Create;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+
+namespace ApiGameVr.Test
+{
+    public class AdminUserControllerTests
+    {
+        [Fact]
+        public async Task CreatingAdminUserShouldReturnUserOk()
+        {
+            await using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+            var requestData = new { email = "test3@test.fr", password = "Azerty12345!" };
+            var response = await client.PostAsJsonAsync("/admin/register",
+                requestData,
+                cancellationToken: TestContext.Current.CancellationToken
+                );
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            await using var application2 = new WebApplicationFactory<Program>();
+            using var client2 = application2.CreateClient();
+            var response2 = await client.PostAsJsonAsync("/admin/login",
+                requestData,
+                cancellationToken: TestContext.Current.CancellationToken
+                );
+            Assert.Equal(HttpStatusCode.OK, response2.StatusCode);
+        }
+    }
+}
